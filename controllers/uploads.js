e6abf993dc7a35d3c6b15544df930175661f3502
nicovidal <nico.vidal.m@gmail.com>
2024-11-04 15:41:10 -0300

    fin backend server cloudinary

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,6 +1,9 @@
 const path = require('path')
 const fs = require('fs')
 
+const cloudinary=require('cloudinary').v2
+cloudinary.config(process.env.CLOUDINARY_URL)
+
 const { response } = require("express");
 const { subirArchivos } = require("../helpers/subir-archivo");
 const { Usuario, Producto } = require("../models");
@@ -90,6 +93,63 @@ const actualizarImagen = async (req, res = response) => {
 
 }
 
+
+const actualizarImagenCloudinary = async (req, res = response) => {
+
+
+    const { id, coleccion } = req.params
+
+
+    let modelo;
+
+    switch (coleccion) {
+        case 'usuarios':
+            modelo = await Usuario.findById(id)
+            if (!modelo) {
+                return res.status(400).json({
+                    msg: `No existe un usuario con el id ${id}`
+                })
+            }
+            break;
+        case 'productos':
+            modelo = await Producto.findById(id)
+            if (!modelo) {
+                return res.status(400).json({
+                    msg: `No existe un producto con el id ${id}`
+                })
+            }
+
+            break;
+        default:
+            return res.status(500).json({ msg: 'se me olvido validar esto ' })
+    }
+
+    // Limpiar imágenes previas
+    if (modelo.img) {
+        const nombreArr=modelo.img.split('/')
+        const nombre =nombreArr[nombreArr.length-1]
+        const [public_id]=nombre.split('.')
+        await cloudinary.uploader.destroy(public_id)
+        
+    }
+
+    const {tempFilePath}=req.files.archivo
+    const {secure_url} = await cloudinary.uploader.upload(tempFilePath)
+/* 
+    const nombre = await subirArchivos(req.files, undefined, coleccion);
+    modelo.img = nombre
+    await modelo.save() */
+
+    modelo.img=secure_url
+    await modelo.save()
+
+
+    res.json(modelo)
+
+
+
+}
+
 const mostrarImagen = async(req, res = response ) => {
 
     const { id, coleccion } = req.params;
@@ -139,5 +199,6 @@ const mostrarImagen = async(req, res = response ) => {
 module.exports = {
     cargarArchivo,
     actualizarImagen,
-    mostrarImagen
+    mostrarImagen,
+    actualizarImagenCloudinary
 }
